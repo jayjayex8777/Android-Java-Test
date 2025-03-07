0124f032d417b8761e79fe7d833b5ca6afc268c2
Intae Jun <jayjayex8777@gmail.com>
2025-03-07 13:29:00 +0900

    Update MainActivity.java
    중앙 정렬

--- objectselect2/MainActivity.java
+++ objectselect2/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,18 +18,23 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        // RecyclerView 설정
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager(layoutManager);
 
-        // 데이터 생성 (1번부터 30번까지)
+        // 데이터 생성 (1번부터 30번까지, "번호 " 제거)
         List<String> dataList = new ArrayList<>();
         for (int i = 1; i <= 30; i++) {
-            dataList.add("번호 " + i);
+            dataList.add(String.valueOf(i));  // 숫자만 표시
         }
 
         // 어댑터 설정
         RectangleAdapter adapter = new RectangleAdapter(dataList);
         recyclerView.setAdapter(adapter);
+
+        // ⭐ LinearSnapHelper 추가 (스크롤 후 항상 중앙 정렬)
+        SnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(recyclerView);
     }
 }
