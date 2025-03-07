bc2bf28332d7f3c1fc1211fc5f5f2d83d227caf9
Intae Jun <jayjayex8777@gmail.com>
2025-03-07 11:15:18 +0900

    Update MainActivity.java

--- objectselect2/MainActivity.java
+++ objectselect2/MainActivity.java
@@ -1,25 +1,33 @@
 package com.example.objectselect2;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-  
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    EdgeToEdge.enable(this);
-    setContentView(R.layout.activity_main);
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main),(v,insets) -> {
-      Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-      return insets;
-    });
-  }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // 데이터 생성 (1번부터 30번까지)
+        List<String> dataList = new ArrayList<>();
+        for (int i = 1; i <= 30; i++) {
+            dataList.add("번호 " + i);
+        }
+
+        // 어댑터 설정
+        RectangleAdapter adapter = new RectangleAdapter(dataList);
+        recyclerView.setAdapter(adapter);
+    }
 }
