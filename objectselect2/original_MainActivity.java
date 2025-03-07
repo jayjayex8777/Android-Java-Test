3f384c4e915282d441f92b46d82e920e39d0705a
Intae Jun <jayjayex8777@gmail.com>
2025-03-07 11:04:37 +0900

    Update original_MainActivity.java

--- objectselect2/original_MainActivity.java
+++ objectselect2/original_MainActivity.java
@@ -1,3 +1,14 @@
+package com.example.objectselect2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
 public class MainActivity extends AppCompatActivity {
   
   @Override
@@ -5,10 +16,10 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     EdgeToEdge.enable(this);
     setContentView(R.layout.activity_main);
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main),(v,insets) -> {
       Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-      return insets;  // 'retrun' → 'return'으로 수정
+      return insets;
     });
   }
 }
