cc4c2af113e6f54c4d116ab3f315414ba0e84e7a
Intae Jun <jayjayex8777@gmail.com>
2025-03-11 15:00:47 +0900

    Update DetailActivity.java

--- ObjectSelect3/DetailActivity.java
+++ ObjectSelect3/DetailActivity.java
@@ -10,9 +10,6 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        
         String coordinate = getIntent().getStringExtra("coordinate");
-        TextView detailTextView = findViewById(R.id.detailTextView);
-        detailTextView.setText(coordinate);
-    }
-}
+        TextView detailTextView = findViewBy
