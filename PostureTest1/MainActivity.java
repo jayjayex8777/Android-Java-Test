e3cdb87ac4bb030d4ffb145aaaa1b77d4f621e7a
Intae Jun <jayjayex8777@gmail.com>
2025-03-12 17:51:06 +0900

    Update MainActivity.java

--- PostureTest1/MainActivity.java
+++ PostureTest1/MainActivity.java
@@ -1,25 +1,28 @@
 package com.example.posturetest1;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor accelerometer, gyroscope;
-    private SmartphoneView smartphoneView; // 스마트폰을 그릴 커스텀 뷰
-
+    private SmartphoneView smartphoneView;
+    private TextView gyroTextView, accelTextView;
+    private GraphView gyroGraph, accelGraph;
+    private LineGraphSeries<DataPoint> gyroYawSeries, gyroPitchSeries, gyroRollSeries;
+    private LineGraphSeries<DataPoint> accelXSeries, accelYSeries, accelZSeries;
     private int graphXIndex = 0;
 
     @Override
@@ -30,21 +33,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         // UI 요소 연결
         smartphoneView = findViewById(R.id.smartphoneView);
+        gyroTextView = findViewById(R.id.gyroTextView);
+        accelTextView = findViewById(R.id.accelTextView);
+        gyroGraph = findViewById(R.id.gyroGraph);
+        accelGraph = findViewById(R.id.accelGraph);
 
-        // RecyclerView 설정
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recyclerView.setLayoutManager(layoutManager);
+        // 그래프 초기화
+        gyroYawSeries = new LineGraphSeries<>();
+        gyroPitchSeries = new LineGraphSeries<>();
+        gyroRollSeries = new LineGraphSeries<>();
+        gyroYawSeries.setColor(Color.RED);
+        gyroPitchSeries.setColor(Color.GREEN);
+        gyroRollSeries.setColor(Color.BLUE);
+        gyroGraph.addSeries(gyroYawSeries);
+        gyroGraph.addSeries(gyroPitchSeries);
+        gyroGraph.addSeries(gyroRollSeries);
 
-        // 데이터 생성
-        List<String> dataList = new ArrayList<>();
-        for (int i = 1; i <= 30; i++) {
-            dataList.add(String.valueOf(i));
-        }
-
-        // 어댑터 설정
-        RectangleAdapter adapter = new RectangleAdapter(dataList);
-        recyclerView.setAdapter(adapter);
+        accelXSeries = new LineGraphSeries<>();
+        accelYSeries = new LineGraphSeries<>();
+        accelZSeries = new LineGraphSeries<>();
+        accelXSeries.setColor(Color.RED);
+        accelYSeries.setColor(Color.GREEN);
+        accelZSeries.setColor(Color.BLUE);
+        accelGraph.addSeries(accelXSeries);
+        accelGraph.addSeries(accelYSeries);
+        accelGraph.addSeries(accelZSeries);
 
         // 센서 매니저 설정
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -60,11 +73,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (sensorManager != null) {
             if (gyroscope != null) {
                 sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_UI);
-                Log.d("SENSOR_REGISTER", "Gyroscope registered successfully");
             }
             if (accelerometer != null) {
                 sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
-                Log.d("SENSOR_REGISTER", "Accelerometer registered successfully");
             }
         }
     }
@@ -72,12 +83,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
         runOnUiThread(() -> {
-            if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-                float pitch = event.values[1]; // 앞뒤 기울기
-                float roll = event.values[2];  // 좌우 기울기
+            graphXIndex++;
 
-                // 스마트폰 이미지 회전 업데이트
+            if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+                float pitch = event.values[1];
+                float roll = event.values[2];
+
                 smartphoneView.updateRotation(pitch, roll);
+                gyroTextView.setText(String.format("Pitch: %+06.2f, Roll: %+06.2f", pitch, roll));
+            }
+
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                float accelX = event.values[0];
+                float accelY = event.values[1];
+                float accelZ = event.values[2];
+
+                accelTextView.setText(String.format("Accel X: %+06.2f, Y: %+06.2f, Z: %+06.2f", accelX, accelY, accelZ));
             }
         });
     }
