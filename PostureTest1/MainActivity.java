77692bb15238e1247d8a551a3884431b01e65528
Intae Jun <jayjayex8777@gmail.com>
2025-03-12 20:19:34 +0900

    Update MainActivity.java

--- PostureTest1/MainActivity.java
+++ PostureTest1/MainActivity.java
@@ -1,79 +1,67 @@
 package com.example.posturetest1;
 
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor accelerometer, gyroscope;
-    private float[] rotationMatrix = new float[9];
-    private float[] orientationValues = new float[3];
+    private GLSurfaceView glSurfaceView;
+    private CubeRenderer cubeRenderer;
+    private TextView sensorTextView;
 
-    private float pitch = 0, roll = 0;
-
-    private SurfaceView surfaceView;
-    private SurfaceHolder holder;
-    private Paint paint;
+    private float yaw = 0, pitch = 0, roll = 0;  // 회전값
+    private float posX = 0, posY = 0, posZ = 0;  // 위치값
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        surfaceView = findViewById(R.id.surfaceView);
-        holder = surfaceView.getHolder();
-        paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.FILL);
+        glSurfaceView = findViewById(R.id.glSurfaceView);
+        sensorTextView = findViewById(R.id.sensorTextView);
+        glSurfaceView.setEGLContextClientVersion(2);
+        
+        cubeRenderer = new CubeRenderer();
+        glSurfaceView.setRenderer(cubeRenderer);
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
         if (accelerometer != null) {
-            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
         }
         if (gyroscope != null) {
-            sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_UI);
+            sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_GAME);
         }
-
-        holder.addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                draw();
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                draw();
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-            }
-        });
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float[] gravity = event.values.clone();
-            SensorManager.getRotationMatrix(rotationMatrix, null, gravity, new float[]{0, 0, 1});
-            SensorManager.getOrientation(rotationMatrix, orientationValues);
-            pitch = (float) Math.toDegrees(orientationValues[1]);  // X 축 회전
-            roll = (float) Math.toDegrees(orientationValues[2]);   // Y 축 회전
+        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            yaw += event.values[2] * 0.1f;  // Z 축 회전
+            pitch += event.values[1] * 0.1f;  // Y 축 회전
+            roll += event.values[0] * 0.1f;  // X 축 회전
         }
-        draw();
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            posX = event.values[0] * 0.1f;
+            posY = event.values[1] * 0.1f;
+            posZ = event.values[2] * 0.1f;
+        }
+
+        cubeRenderer.setTransform(yaw, pitch, roll, posX, posY, posZ);
+        sensorTextView.setText(String.format("Yaw: %+.2f, Pitch: %+.2f, Roll: %+.2f\nAccel X: %+.2f, Y: %+.2f, Z: %+.2f",
+                yaw, pitch, roll, posX, posY, posZ));
     }
 
     @Override
@@ -84,26 +72,5 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onDestroy() {
         super.onDestroy();
         sensorManager.unregisterListener(this);
-    }
-
-    private void draw() {
-        Canvas canvas = holder.lockCanvas();
-        if (canvas != null) {
-            canvas.drawColor(Color.WHITE);
-            canvas.save();
-
-            float centerX = surfaceView.getWidth() / 2.0f;
-            float centerY = surfaceView.getHeight() / 2.0f;
-            float rectWidth = 300;
-            float rectHeight = 600;
-
-            canvas.translate(centerX, centerY);
-            canvas.rotate(roll);
-
-            canvas.drawRect(-rectWidth / 2, -rectHeight / 2, rectWidth / 2, rectHeight / 2, paint);
-            canvas.restore();
-
-            holder.unlockCanvasAndPost(canvas);
-        }
     }
 }
