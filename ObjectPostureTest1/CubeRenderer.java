f4bc0c906986247bdff98c564585040309768242
Intae Jun <jayjayex8777@gmail.com>
2025-03-13 15:41:14 +0900

    Update CubeRenderer.java

--- ObjectPostureTest1/CubeRenderer.java
+++ ObjectPostureTest1/CubeRenderer.java
@@ -1,4 +1,4 @@
-package objectposturetest1;
+package com.example.objectposturetest1;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
@@ -16,7 +16,7 @@ import javax.microedition.khronos.opengles.GL10;
 public class CubeRenderer implements GLSurfaceView.Renderer {
     private FloatBuffer vertexBuffer;
     private ShortBuffer drawListBuffer;
-    
+
     private final String vertexShaderCode =
             "attribute vec4 vPosition;" +
             "uniform mat4 uMVPMatrix;" +
@@ -59,6 +59,8 @@ public class CubeRenderer implements GLSurfaceView.Renderer {
     };
 
     public CubeRenderer() {
+        Log.d("GLRenderer", "Initializing CubeRenderer");
+
         ByteBuffer bb = ByteBuffer.allocateDirect(cubeCoords.length * 4);
         bb.order(ByteOrder.nativeOrder());
         vertexBuffer = bb.asFloatBuffer();
@@ -75,30 +77,46 @@ public class CubeRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        GLES20.glClearColor(1f, 1f, 1f, 1f);
+        Log.d("GLRenderer", "onSurfaceCreated called");
+
+        GLES20.glClearColor(1f, 1f, 1f, 1f); // 🚀 배경 흰색 설정
         GLES20.glEnable(GLES20.GL_DEPTH_TEST);
-        
+
         program = GLES20.glCreateProgram();
         int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
         int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+
+        if (vertexShader == 0 || fragmentShader == 0) {
+            Log.e("GLRenderer", "Error: Shader compilation failed!");
+            return;
+        }
+
         GLES20.glAttachShader(program, vertexShader);
         GLES20.glAttachShader(program, fragmentShader);
         GLES20.glLinkProgram(program);
+
+        int[] linked = new int[1];
+        GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, linked, 0);
+        if (linked[0] == 0) {
+            Log.e("GLRenderer", "Error: Shader program linking failed!");
+            GLES20.glDeleteProgram(program);
+            program = 0;
+        }
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
+        Log.d("GLRenderer", "onDrawFrame called");
+
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
         GLES20.glUseProgram(program);
 
         positionHandle = GLES20.glGetAttribLocation(program, "vPosition");
-        if (positionHandle == -1) {
-            Log.e("GLRenderer", "Error: vPosition not found in shader");
-        }
-
         colorHandle = GLES20.glGetUniformLocation(program, "vColor");
-        if (colorHandle == -1) {
-            Log.e("GLRenderer", "Error: vColor not found in shader");
+
+        if (drawListBuffer == null) {
+            Log.e("GLRenderer", "Error: drawListBuffer is null!");
+            return;
         }
 
         GLES20.glEnableVertexAttribArray(positionHandle);
@@ -109,11 +127,6 @@ public class CubeRenderer implements GLSurfaceView.Renderer {
         Matrix.rotateM(modelMatrix, 0, rotationX, 0, 1, 0);
         Matrix.rotateM(modelMatrix, 0, rotationY, 1, 0, 0);
 
-        if (drawListBuffer == null) {
-            Log.e("GLRenderer", "Error: drawListBuffer is null!");
-            return;
-        }
-
         GLES20.glDrawElements(GLES20.GL_TRIANGLES, drawOrder.length, GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
         GLES20.glDisableVertexAttribArray(positionHandle);
     }
@@ -123,9 +136,6 @@ public class CubeRenderer implements GLSurfaceView.Renderer {
         GLES20.glViewport(0, 0, width, height);
     }
 
-    /**
-     * 🚀 setRotation() 추가: 터치 입력을 받아 3D 객체 회전 처리
-     */
     public void setRotation(float dx, float dy) {
         rotationX += dx * 0.5f;
         rotationY += dy * 0.5f;
@@ -136,12 +146,10 @@ public class CubeRenderer implements GLSurfaceView.Renderer {
         GLES20.glShaderSource(shader, shaderCode);
         GLES20.glCompileShader(shader);
 
-        // 🚨 쉐이더 컴파일 에러 확인
         int[] compiled = new int[1];
         GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0);
         if (compiled[0] == 0) {
             Log.e("Shader", "Could not compile shader " + type + ":");
-            Log.e("Shader", GLES20.glGetShaderInfoLog(shader));
             GLES20.glDeleteShader(shader);
             return 0;
         }
