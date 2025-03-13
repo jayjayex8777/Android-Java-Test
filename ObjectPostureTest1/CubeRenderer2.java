0539c41751bae5c1227f19f803d2277d9a055bf4
Intae Jun <jayjayex8777@gmail.com>
2025-03-13 18:23:36 +0900

    Create CubeRenderer2.java

---
+++ ObjectPostureTest1/CubeRenderer2.java
@@ -0,0 +1,56 @@
+package com.example.objectposturetest1;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class CubeRenderer implements GLSurfaceView.Renderer {
+    private FloatBuffer vertexBuffer;
+    private ShortBuffer drawListBuffer;
+
+    private int program;
+    private int positionHandle, colorHandle, mvpMatrixHandle;
+    private float[] projectionMatrix = new float[16];
+    private float[] viewMatrix = new float[16];
+    private float[] modelMatrix = new float[16];
+    private float[] mvpMatrix = new float[16];
+
+    private float rotationX = 0;
+    private float rotationY = 0;
+
+    public void setRotation(float dx, float dy) {
+        rotationX += dy * 0.5f;
+        rotationY += dx * 0.5f;
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+        float ratio = (float) width / height;
+
+        Matrix.perspectiveM(projectionMatrix, 0, 45, ratio, 1, 10);
+        Matrix.setLookAtM(viewMatrix, 0, 0, 0, 5, 0, 0, 0, 0, 1, 0);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        Matrix.setIdentityM(modelMatrix, 0);
+        Matrix.rotateM(modelMatrix, 0, rotationX, 1, 0, 0);
+        Matrix.rotateM(modelMatrix, 0, rotationY, 0, 1, 0);
+
+        Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+        Matrix.multiplyMM(mvpMatrix, 0, mvpMatrix, 0, modelMatrix, 0);
+
+        GLES20.glUniformMatrix4fv(mvpMatrixHandle, 1, false, mvpMatrix, 0);
+    }
+}
