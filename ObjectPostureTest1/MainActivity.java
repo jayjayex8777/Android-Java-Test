1148858f790101ebed2eff6e933f108e7c10d974
Intae Jun <jayjayex8777@gmail.com>
2025-03-13 14:22:49 +0900

    Update MainActivity.java

--- ObjectPostureTest1/MainActivity.java
+++ ObjectPostureTest1/MainActivity.java
@@ -1,78 +1,42 @@
-package com.example.objectposturetest1;
+package objectposturetest1;
 
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.View;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     private GLSurfaceView glSurfaceView;
-    private PrismRenderer renderer;
-    private float previousX;
-    private float previousY;
-    private static final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+    private CubeRenderer renderer;
+    private float previousX, previousY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-
         glSurfaceView = new GLSurfaceView(this);
-        glSurfaceView.setEGLContextClientVersion(2); // OpenGL ES 2.0
-        renderer = new PrismRenderer();
+        glSurfaceView.setEGLContextClientVersion(2); // OpenGL ES 2.0 사용
+        renderer = new CubeRenderer();
         glSurfaceView.setRenderer(renderer);
-        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY); // 터치 시 렌더링
-
-        glSurfaceView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                float x = event.getX();
-                float y = event.getY();
-
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        previousX = x;
-                        previousY = y;
-                        return true;
-                    case MotionEvent.ACTION_MOVE:
-                        float dx = x - previousX;
-                        float dy = y - previousY;
-                        renderer.setRotationY(renderer.getRotationY() + (dx * TOUCH_SCALE_FACTOR));
-                        renderer.setRotationX(renderer.getRotationX() + (dy * TOUCH_SCALE_FACTOR));
-                        glSurfaceView.requestRender();
-                        break;
-                }
-                previousX = x;
-                previousY = y;
-                return true;
-            }
-        });
-
         setContentView(glSurfaceView);
-
-        ViewCompat.setOnApplyWindowInsetsListener(glSurfaceView, (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        glSurfaceView.onResume();
-    }
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        glSurfaceView.onPause();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                float dx = x - previousX;
+                float dy = y - previousY;
+
+                renderer.setRotation(dx, dy);
+                glSurfaceView.requestRender();
+                break;
+        }
+
+        previousX = x;
+        previousY = y;
+        return true;
     }
 }
