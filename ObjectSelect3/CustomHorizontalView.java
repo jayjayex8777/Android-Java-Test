0adcedd1492d402cb6e936cd18a31444f8dbabf8
Intae Jun <jayjayex8777@gmail.com>
2025-03-18 19:19:57 +0900

    Update CustomHorizontalView.java

--- ObjectSelect3/CustomHorizontalView.java
+++ ObjectSelect3/CustomHorizontalView.java
@@ -2,7 +2,7 @@ package com.example.objectselect3;
 
 import android.content.Context;
 import android.os.Handler;
-import android.os.HandelrThread;
+import android.os.HandlerThread;
 import android.os.Looper;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -10,18 +10,20 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.HorizontalScrollView;
 
-import android.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import java.util.logging.LogRecord;
 
-public calss CustomHorizontalScroolView extends HorizontalScroolView {
+public class CustomHorizontalScroolView extends HorizontalScroolView {
   private float lastX = 0.0f;
-  privata float lastY = 0.0f;
+  private float lastY = 0.0f;
   private boolean scrolling = false;
 
 private HandlerThread scrollHandler = new HandlerThread ("Thread");
 
-public CustomHorizontalScrollView(Context context){ this(context, null);}
+public CustomHorizontalScrollView(Context context){ 
+  this(context, null);
+}
 
 public CustomHorizontalScrollView(Context context, AttributeSet attrs) {
   this(context, attrs,0);
@@ -44,17 +46,17 @@ protected void onScrollChanged(int l, int t, int oldl, int oldt) {
        Log.d("VESW", "MOTION scrolling true");
        scrolling = true;
    } else {
-       Log.d("VESW", "MOTION scrolling true");
+       Log.d("VESW", "MOTION scrolling false");
        scrolling = false;
    }
 }
 
 @Override
-public boolean onInterceptTouchEvnet(MotionEvent e) {
-  Log.d("VESW", "ACTION" + e.getActionMaseked());
+public boolean onInterceptTouchEvent(MotionEvent e) {
+  Log.d("VESW", "ACTION :" + e.getActionMaseked());
   boolean allowScroll = false;
   switch (e.getActionMasked()){
-    case MotionEvnet.ACTION_DOWM:
+    case MotionEvent.ACTION_DOWN:
         lastX = e.getX();
         lastY = e.getY();
         Log.d("VESW","MOTION ACTION DOWN");
@@ -62,7 +64,7 @@ public boolean onInterceptTouchEvnet(MotionEvent e) {
           scrolling = false;
           Log.d("VESW","MOTION Virtual ACITON DOWN");
           MotionEvent ev = MotionEvent.obtain(e);
-          ev.setAction(MotionEvnet.ACTION_UP)
+          ev.setAction(MotionEvent.ACTION_UP)
           super.onInterceptTouchEvent(ev);
         }
         break;
@@ -75,8 +77,9 @@ public boolean onInterceptTouchEvnet(MotionEvent e) {
       Log.d("VESW", String.format("ALLOW : %s, [dx : %f], [dy :%f]", allowScroll? "allow" : "not allow",dx,dy));
 
       break;
-    case MotionEvent.ACTION_UP
+    case MotionEvent.ACTION_UP:
       Log.d("VESW","MOTION ACTION UP");
+      break;
   }
 
   if (scrolling || !allowScroll) {
