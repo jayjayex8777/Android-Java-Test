0cbd710b75b9e3ad1ee060c09eebb5d3cf65f8d6
Intae Jun <jayjayex8777@gmail.com>
2025-04-22 21:06:41 +0900

    Update MainActivity.java

--- ObjectSelect3/MainActivity.java
+++ ObjectSelect3/MainActivity.java
@@ -1,4 +1,4 @@
-*package com.example.objectselect3;
+package com.example.objectselect3;
 
 import android.Manifest; import android.content.Intent; import android.content.pm.PackageManager; import android.net.Uri; import android.os.Build; import android.os.Bundle; import android.os.Environment; import android.provider.Settings; import android.view.MotionEvent; import android.widget.Button; import android.widget.TextView; import android.widget.Toast;
 
